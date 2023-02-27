fea9d777b07112c242ae36de3dc8b58c23c8496a
ilsasdo <ilsasdo@gmail.com>
2023-02-27 16:19:48 +0100

    instruction refactor and register operations

--- src/instruction.rs
+++ src/instruction.rs
@@ -21,12 +21,12 @@ impl Instruction {
         };
     }
 
-    pub fn byte_sum_3(second_nibble: u8, third_nibble: u8, fourth_nibble: u8) -> u16 {
-        return ((second_nibble as u16) << 8) + ((third_nibble as u16) << 4) + (fourth_nibble as u16);
+    pub fn byte_sum_3(&self) -> u16 {
+        return ((self.second_nibble as u16) << 8) + ((self.third_nibble as u16) << 4) + (self.fourth_nibble as u16);
     }
 
-    pub fn byte_sum_2(third_nibble: u8, fourth_nibble: u8) -> u8 {
-        return (third_nibble << 4) + fourth_nibble;
+    pub fn byte_sum_2(&self) -> u8 {
+        return (self.third_nibble << 4) + self.fourth_nibble;
     }
 
     pub fn to_string(&self) -> String {
@@ -52,6 +52,7 @@ mod tests {
     fn build_instruction_from_nibbles() {
         // assert_eq!(3, Instruction::byte_sum_3(0x0, 0x0, 0x3));
         // assert_eq!(0x23, Instruction::byte_sum_3(0x0, 0x2, 0x3));
-        assert_eq!(228, Instruction::byte_sum_3((2 as u8), (2 as u8), (8 as u8)));
+        let i : Instruction = Instruction::new(0x12, 0x28);
+        assert_eq!(0x228, i.byte_sum_3());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,11 @@ struct Chip8 {
     delay_timer: u8,
     sound_timer: u8,
     registers: [u8; 16],
+    options: Chip8Options,
+}
+
+struct Chip8Options {
+    super_chip: bool,
 }
 
 impl Chip8 {
@@ -28,6 +33,9 @@ impl Chip8 {
             delay_timer: 0,
             sound_timer: 0,
             registers: [0x0; 16],
+            options: Chip8Options {
+                super_chip: false
+            },
         };
 
         let fonts: [u8; 80] = [0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -120,7 +128,7 @@ impl Chip8 {
         self.pc = location;
     }
 
-    fn set_register(&mut self, register: u8, value: u8) {
+    fn register_set_value(&mut self, register: u8, value: u8) {
         self.registers[usize::from(register)] = value;
     }
 
@@ -128,8 +136,59 @@ impl Chip8 {
         self.i = value;
     }
 
-    fn add_to_register(&mut self, register: u8, value: u8) {
+    fn register_add_value(&mut self, register: u8, value: u8) {
         self.registers[usize::from(register)] += value;
+    }
+
+    fn register_set(&mut self, register_a: u8, register_b: u8) {
+        self.registers[register_a as usize] = self.registers[register_b as usize];
+    }
+
+    fn register_get_value(self, register: u8) -> u8 {
+        return self.registers[register as usize];
+    }
+
+    fn register_or(&mut self, register_a: u8, register_b: u8) {
+        self.registers[register_a as usize] = self.registers[register_a as usize] | self.registers[register_b as usize];
+    }
+
+    fn register_and(&mut self, register_a: u8, register_b: u8) {
+        self.registers[register_a as usize] = self.registers[register_a as usize] & self.registers[register_b as usize];
+    }
+
+    fn register_xor(&mut self, register_a: u8, register_b: u8) {
+        self.registers[register_a as usize] = self.registers[register_a as usize] ^ self.registers[register_b as usize];
+    }
+
+    fn register_add(&mut self, register_a: u8, register_b: u8) {
+        let a = self.registers[register_a as usize] as u16;
+        let b = self.registers[register_b as usize] as u16;
+
+        self.registers[register_a as usize] = (a + b) as u8;
+        self.registers[0xF] = if (a + b) > 255u16 { 1 } else { 0 };
+    }
+
+    fn register_subtract(&mut self, register_a: u8, register_b: u8) {
+        let value_a = self.registers[register_a as usize];
+        let value_b = self.registers[register_b as usize];
+        self.registers[0xF] = if value_a >= value_b { 1 } else { 0 };
+        self.registers[register_a as usize] = value_a - value_b;
+    }
+
+    fn register_left_shift(&mut self, register_a: u8, register_b: u8) {
+        if self.options.super_chip {
+            self.register_set(register_a, register_b);
+        }
+
+        self.register_set_value(register_a, self.register_get_value(register_a) << 1);
+    }
+
+    fn register_right_shift(&mut self, register_a: u8, register_b: u8) {
+        if self.options.super_chip {
+            self.register_set(register_a, register_b);
+        }
+
+        self.register_set_value(register_a, self.register_get_value(register_a) >> 1);
     }
 
     fn draw(&mut self, x_register: u8, y_register: u8, height: u8) {
@@ -188,8 +247,6 @@ impl Chip8 {
             // read the instruction pointed from the pc:
             let instruction = self.fetch_instruction()?;
 
-            // println!("DEBUG Instruction: {}, pc: {}", instruction.to_string(), self.pc);
-
             match instruction.first_nibble {
                 0x0 => {
                     match instruction.second_nibble {
@@ -215,19 +272,19 @@ impl Chip8 {
                 }
 
                 0x1 => {
-                    self.jump(Instruction::byte_sum_3(instruction.second_nibble, instruction.third_nibble, instruction.fourth_nibble));
+                    self.jump(instruction.byte_sum_3());
                 }
 
                 0x2 => {
-                    self.call_subroutine(Instruction::byte_sum_3(instruction.second_nibble, instruction.third_nibble, instruction.fourth_nibble));
+                    self.call_subroutine(instruction.byte_sum_3());
                 }
 
                 0x3 => {
-                    self.skip_if_equals(self.registers[instruction.second_nibble as usize], Instruction::byte_sum_2(instruction.third_nibble, instruction.fourth_nibble));
+                    self.skip_if_equals(self.registers[instruction.second_nibble as usize], instruction.byte_sum_2());
                 }
 
                 0x4 => {
-                    self.skip_if_not_equals(self.registers[instruction.second_nibble as usize], Instruction::byte_sum_2(instruction.third_nibble, instruction.fourth_nibble));
+                    self.skip_if_not_equals(self.registers[instruction.second_nibble as usize], instruction.byte_sum_2());
                 }
 
                 0x5 => {
@@ -235,11 +292,41 @@ impl Chip8 {
                 }
 
                 0x6 => {
-                    self.set_register(instruction.second_nibble, Instruction::byte_sum_2(instruction.third_nibble, instruction.fourth_nibble));
+                    self.register_set_value(instruction.second_nibble, instruction.byte_sum_2());
                 }
 
                 0x7 => {
-                    self.add_to_register(instruction.second_nibble, Instruction::byte_sum_2(instruction.third_nibble, instruction.fourth_nibble));
+                    self.register_add_value(instruction.second_nibble, instruction.byte_sum_2());
+                }
+
+                0x8 => {
+                    match instruction.fourth_nibble {
+                        0x0 => {
+                            self.register_set(instruction.second_nibble, instruction.third_nibble);
+                        }
+                        0x1 => {
+                            self.register_or(instruction.second_nibble, instruction.third_nibble);
+                        }
+                        0x2 => {
+                            self.register_and(instruction.second_nibble, instruction.third_nibble);
+                        }
+                        0x3 => {
+                            self.register_xor(instruction.second_nibble, instruction.third_nibble);
+                        }
+                        0x4 => {
+                            self.register_add(instruction.second_nibble, instruction.third_nibble);
+                        }
+                        0x5 => {
+                            self.register_subtract(instruction.second_nibble, instruction.third_nibble);
+                        }
+                        0x6 => {
+                            self.register_right_shift(instruction.third_nibble, instruction.second_nibble);
+                        }
+                        0xE => {
+                            self.register_left_shift(instruction.second_nibble, instruction.third_nibble);
+                        }
+                        _ => {}
+                    }
                 }
 
                 0x9 => {
@@ -247,7 +334,10 @@ impl Chip8 {
                 }
 
                 0xA => {
-                    self.set_index_register(Instruction::byte_sum_3(instruction.second_nibble, instruction.third_nibble, instruction.fourth_nibble));
+                    self.set_index_register(instruction.byte_sum_3());
+                }
+
+                0xB => {
                 }
 
                 0xD => {
@@ -267,4 +357,19 @@ fn main() {
     let mut chip8 = Chip8::new();
     chip8.load_rom(String::from("roms/IBM Logo.ch8")).expect("File to exists.");
     chip8.execute().expect("OH NO!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn add_overflow() {
+        let a: u8 = 244u8;
+        let b: u8 = 244u8;
+
+        if ((a as u16) + (b as u16)) > 255u16 {
+            assert!(true)
+        } else {
+            assert!(false)
+        }
+    }
 }
